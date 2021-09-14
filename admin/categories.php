e29579637a3722208073aac0c1fd56f0213cc967
g0spon <bricha.prude@gmail.com>
2021-09-14 19:22:27 +0200

    adding category

--- admin/categories.php
+++ admin/categories.php
@@ -18,7 +18,37 @@
                     </h1>
 
                     <div class="col-xs-6">
-                    <form action=""></form>
+
+                        <?php
+
+                        if (isset($_POST['submit'])) {
+
+                            $cat_title = $_POST['cat_title'];
+
+                            if ($cat_title == "" || empty($cat_title)) {
+
+                                echo "This field should not be empty";
+
+                            } else {
+
+                                $query = "INSERT INTO categories(cat_title) ";
+                                $query .= "VALUE('{$cat_title}') ";
+
+                                $create_category_query = mysqli_query($connection, $query);
+
+                                if (!$create_category_query) {
+
+                                    die('QUERY FAILED' . mysqli_error($connection));
+
+                                }
+
+                            }
+
+                        }
+
+                        ?>
+
+                    <form action="" method="post">
                         <div class="form-group">
                             <label for="cat_title">Add Category</label>
                             <input class="form-control" type="text" name="cat_title">
@@ -26,6 +56,7 @@
                         <div class="form-group">
                             <input class="btn btn-primary" type="submit" name="submit" value="Add category">
                         </div>
+                    </form>
                     </div> <!--Add Category Form-->
 
                     <div class="col-xs-6">
