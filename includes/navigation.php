5413e2ce403e914698c39fe76226103abb383c73
g0spon <bricha.prude@gmail.com>
2021-09-14 17:52:23 +0200

    admin frontend tiny fixes

--- includes/navigation.php
+++ includes/navigation.php
@@ -29,20 +29,9 @@
 
                 ?>
 
-
-
-
-
-
-                <!--<li>
-                    <a href="#">About</a>
-                </li>
                 <li>
-                    <a href="#">Services</a>
+                    <a href="#">Admin</a>
                 </li>
-                <li>
-                    <a href="#">Contact</a>
-                </li>-->
             </ul>
         </div>
         <!-- /.navbar-collapse -->
