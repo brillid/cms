8cab187a29e1976e5483f574e9b92b3dcdcfd1cb
g0spon <bricha.prude@gmail.com>
2021-09-26 22:53:45 +0200

    update posts

--- admin/includes/edit_post.php
+++ admin/includes/edit_post.php
@@ -6,7 +6,7 @@
 
     }
 
-    $query = "SELECT * FROM posts";
+    $query = "SELECT * FROM posts WHERE post_id = $the_post_id ";
 
     $select_posts_by_id = mysqli_query($connection, $query);
 
@@ -25,13 +25,55 @@
 
     }
 
+    if (isset($_POST['update_post']))
+    {
+        $post_author = $_POST['post_author'];
+        $post_title = $_POST['post_title'];
+        $post_category_id = $_POST['post_category'];
+        $post_status = $_POST['post_status'];
+        $post_image = $_FILES['image']['name'];
+        $post_image_temp = $_FILES['image']['tmp_name'];
+        $post_content = $_POST['post_content'];
+        $post_tags = $_POST['post_tags'];
+
+        move_uploaded_file($post_image_temp, "../images/$post_image");
+
+        if (empty($post_image))
+        {
+            $query = "SELECT * FROM posts WHERE post_id = $the_post_id ";
+            $select_image = mysqli_query($connection, $query);
+
+            while ($row = mysqli_fetch_array($select_image))
+            {
+                $post_image = $row['post_image'];
+            }
+
+        }
+
+
+        $query = "UPDATE posts SET ";
+        $query .= "post_title = '$post_title', ";
+        $query .= "post_category_id = '$post_category_id', ";
+        $query .= "post_date = now(), ";
+        $query .= "post_author = '$post_author', ";
+        $query .= "post_status = '$post_status', ";
+        $query .= "post_tags = '$post_tags', ";
+        $query .= "post_content = '$post_content', ";
+        $query .= "post_image = '$post_image' ";
+        $query .= "WHERE post_id = $the_post_id ";
+
+        $update_post = mysqli_query($connection, $query);
+
+        confirmQuery($update_post);
+    }
+
 ?>
 
 
 <form action="" method="post" enctype="multipart/form-data">
     <div class="form-group">
         <label for="title">Post Title</label>
-        <input value="<?php echo $post_title; ?>" type="text" class="form-control" name="title">
+        <input value="<?php echo $post_title; ?>" type="text" class="form-control" name="post_title">
     </div>
 
     <div class="form-group">
@@ -60,7 +102,7 @@
 
     <div class="form-group">
         <label for="title">Post Author</label>
-        <input value="<?php echo $post_author; ?>" type="text" class="form-control" name="author">
+        <input value="<?php echo $post_author; ?>" type="text" class="form-control" name="post_author">
     </div>
 
     <div class="form-group">
@@ -70,6 +112,7 @@
 
     <div class="form-group">
         <img width="300" src="../images/<?php echo $post_image; ?>">
+        <input type="file" name="image">
     </div>
 
     <div class="form-group">
@@ -83,6 +126,6 @@
     </div>
 
     <div class="form-group">
-        <input type="submit" class="btn btn-primary" name="create_post" value="Publish Post">
+        <input type="submit" class="btn btn-primary" name="update_post" value="Update Post">
     </div>
 </form>
