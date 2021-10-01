106dbde875bf5bb2942c6d3ad0b5724f019a39cf
g0spon <bricha.prude@gmail.com>
2021-10-01 19:50:17 +0200

    display comments based on approval

--- post.php
+++ post.php
@@ -40,12 +40,12 @@
 
                 <!-- First Blog Post -->
                 <h2>
-                    <a href="#"><?php echo $post_title; ?></a>
+                    <a href="#"><?php echo $post_title ?></a>
                 </h2>
                 <p class="lead">
-                    by <a href="index.php"><?php echo $post_author; ?></a>
+                    by <a href="index.php"><?php echo $post_author ?></a>
                 </p>
-                <p><span class="glyphicon glyphicon-time"></span> <?php echo $post_date; ?></p>
+                <p><span class="glyphicon glyphicon-time"></span> <?php echo $post_date ?></p>
                 <hr>
                 <img class="img-responsive" src="images/<?php echo $post_image; ?>" alt="">
                 <hr>
@@ -74,17 +74,19 @@
                 $create_comment_query = mysqli_query($connection, $query);
 
                 if (!$create_comment_query) {
-                    die('QUERY FAILER' . mysqli_error($connection));
+                    die('QUERY FAILED' . mysqli_error($connection));
                 }
 
             }
 
             ?>
 
+            <!-- Posted Comments -->
+
             <!-- Comments Form -->
             <div class="well">
                 <h4>Leave a Comment:</h4>
-                <form action="" method="post" role="form">
+                <form action="#" method="post" role="form">
                     <div class="form-group">
                         <label for="Author">Author</label>
                         <input type="text" class="form-control" name="comment_author">
@@ -103,46 +105,47 @@
 
             <hr>
 
-            <!-- Posted Comments -->
 
-            <!-- Comment -->
+
+            <?php
+
+            $query = "SELECT * FROM comments WHERE comment_post_id = {$the_post_id} ";
+            $query .= "AND comment_status = 'approved' ";
+            $query .= "ORDER BY comment_id DESC ";
+
+            $select_comment_query = mysqli_query($connection, $query);
+
+            if (!$select_comment_query) {
+
+                die('QUERY FAILED' . mysqli_error($connection));
+
+                }
+
+                while ($row = mysqli_fetch_array($select_comment_query)) {
+
+                    $comment_date = $row['comment_date'];
+                    $comment_content = $row['comment_content'];
+                    $comment_author = $row['comment_author'];
+
+                    ?>
+
+
+                    <!-- Comment -->
             <div class="media">
                 <a class="pull-left" href="#">
                     <img class="media-object" src="http://placehold.it/64x64" alt="">
                 </a>
                 <div class="media-body">
-                    <h4 class="media-heading">Start Bootstrap
-                        <small>August 25, 2014 at 9:30 PM</small>
+                    <h4 class="media-heading"><?php echo $comment_author; ?>
+                    <small><?php echo $comment_date; ?></small>
                     </h4>
-                    Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin commodo. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus.
+                    <?php echo $comment_content; ?>
                 </div>
             </div>
 
-            <!-- Comment -->
-            <div class="media">
-                <a class="pull-left" href="#">
-                    <img class="media-object" src="http://placehold.it/64x64" alt="">
-                </a>
-                <div class="media-body">
-                    <h4 class="media-heading">Start Bootstrap
-                        <small>August 25, 2014 at 9:30 PM</small>
-                    </h4>
-                    Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin commodo. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus.
-                    <!-- Nested Comment -->
-                    <div class="media">
-                        <a class="pull-left" href="#">
-                            <img class="media-object" src="http://placehold.it/64x64" alt="">
-                        </a>
-                        <div class="media-body">
-                            <h4 class="media-heading">Nested Start Bootstrap
-                                <small>August 25, 2014 at 9:30 PM</small>
-                            </h4>
-                            Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin commodo. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus.
-                        </div>
-                    </div>
-                    <!-- End Nested Comment -->
-                </div>
-            </div>
+
+               <?php } ?>
+
 
         </div>
 
@@ -156,5 +159,3 @@
 
     <!-- Footer -->
     <?php include "includes/footer.php"; ?>
-
-
