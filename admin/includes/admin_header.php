d1c3d29e449dce468b0c0c785e783ef38d3e0f85
g0spon <bricha.prude@gmail.com>
2021-10-13 22:58:32 +0200

    validating user admin

--- admin/includes/admin_header.php
+++ admin/includes/admin_header.php
@@ -3,6 +3,20 @@
 <?php ob_start(); ?>
 <?php session_start(); ?>
 
+<?php
+
+if (isset($_SESSION['user_role'])) {
+
+    if ($_SESSION['user_role'] == 'subscriber') {
+
+        header("Location: ../index.php");
+
+    }
+
+}
+
+?>
+
 
 <!DOCTYPE html>
 <html lang="en">
