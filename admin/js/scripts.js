fd171af8924239919745aea7b33be42acd68d387
g0spon <bricha.prude@gmail.com>
2021-10-18 20:03:11 +0200

    adding bulk options to posts in admin

--- admin/js/scripts.js
+++ admin/js/scripts.js
@@ -1,3 +1,17 @@
 $(document).ready(function() {
     $('#summernote').summernote({height: 200});
 });
+
+$(document).ready(function () {
+    $('#selectAllBoxes').click(function (event){
+        if (this.checked) {
+            $('.checkBoxes').each(function (){
+                this.checked = true;
+            });
+        } else {
+            $('.checkBoxes').each(function (){
+                this.checked = false;
+            });
+        }
+    });
+});
