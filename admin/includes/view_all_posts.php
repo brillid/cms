5e0596b1a13a6c399c99db68735a74406dd2a27c
g0spon <bricha.prude@gmail.com>
2021-10-26 18:52:12 +0200

    cloning posts

--- admin/includes/view_all_posts.php
+++ admin/includes/view_all_posts.php
@@ -33,6 +33,32 @@ if (isset($_POST['checkBoxArray'])) {
                 confirmQuery($delete_post_query);
                 break;
 
+            case 'clone':
+                $query = "SELECT * FROM posts WHERE post_id = '$postValueId'";
+
+                $select_post_query = mysqli_query($connection, $query);
+
+                while ($row = mysqli_fetch_array($select_post_query)) {
+
+                    $post_title = $row['post_title'];
+                    $post_category_id = $row['post_category_id'];
+                    $post_date = $row[$post_date];
+                    $post_author = $row['post_author'];
+                    $post_status = $row['post_status'];
+                    $post_image = $row['post_image'];
+                    $post_tags = $row['post_tags'];
+                    $post_content = $row['post_content'];
+
+                }
+
+                $query = "INSERT INTO posts(post_category_id, post_title, post_author, post_date, post_image, post_content, post_tags, post_status) ";
+                $query .= "VALUES($post_category_id, '$post_title', '$post_author', now(), '$post_image', '$post_content', '$post_tags', '$post_status')";
+
+                $copy_query = mysqli_query($connection, $query);
+
+                confirmQuery($copy_query);
+                break;
+
         }
 
     }
@@ -50,6 +76,7 @@ if (isset($_POST['checkBoxArray'])) {
                 <option value="published">Publish</option>
                 <option value="draft">Draft</option>
                 <option value="delete">Delete</option>
+                <option value="clone">Clone</option>
             </select>
         </div>
 
@@ -80,7 +107,7 @@ if (isset($_POST['checkBoxArray'])) {
 
         <?php
 
-        $query = "SELECT * FROM posts";
+        $query = "SELECT * FROM posts ORDER BY post_id DESC ";
 
         $select_posts = mysqli_query($connection, $query);
 
