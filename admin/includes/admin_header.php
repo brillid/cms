9fe2eda5486de490c878cb6c492661f4bb8d1aef
g0spon <bricha.prude@gmail.com>
2021-10-27 18:00:03 +0200

    adding loader to the cms admin

--- admin/includes/admin_header.php
+++ admin/includes/admin_header.php
@@ -33,6 +33,9 @@ if (!isset($_SESSION['user_role'])) {
     <!-- Custom CSS -->
     <link href="css/sb-admin.css" rel="stylesheet">
 
+    <!-- Loader CSS -->
+    <link href="css/loader.css" rel="stylesheet">
+
     <!-- Custom Fonts -->
     <link href="font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
 
