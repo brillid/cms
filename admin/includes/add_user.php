c426bb0d274d1a3f2cb3cd3567b48442ce085d36
g0spon <bricha.prude@gmail.com>
2021-11-22 11:07:24 +0100

    updated add user in admin with new pass system

--- admin/includes/add_user.php
+++ admin/includes/add_user.php
@@ -16,6 +16,8 @@ if (isset($_POST['create_user'])) {
 
 //    move_uploaded_file($post_image_temp, "../images/$post_image" );
 
+    $user_password = password_hash($user_password, PASSWORD_BCRYPT, array('cost' => 10));
+
     $query = "INSERT INTO users(user_firstname, user_lastname, user_role, username, user_email, user_password) VALUES('$user_firstname', '$user_lastname', '$user_role', '$username', '$user_email', '$user_password') ";
 
     $create_user_query = mysqli_query($connection, $query);
