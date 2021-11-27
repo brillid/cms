0474c0ff227ba59173b075841cad664b8bd28dfe
g0spon <bricha.prude@gmail.com>
2021-11-27 23:34:27 +0100

    post comment redirect

--- admin/post_comments.php
+++ admin/post_comments.php
@@ -86,9 +86,9 @@
         }
 
         echo "<td>$comment_date</td>";
-        echo "<td><a href='comments.php?approve=$comment_id'>Approve</a></td>";
-        echo "<td><a href='comments.php?unapprove=$comment_id'>Unapprove</a></td>";
-        echo "<td><a href='comments.php?delete=$comment_id'>Delete</a></td>";
+        echo "<td><a href='post_comments.php?approve=$comment_id&id=" . $_GET['id'] ."'>Approve</a></td>";
+        echo "<td><a href='post_comments.php?unapprove=$comment_id&id=" . $_GET['id'] ."'>Unapprove</a></td>";
+        echo "<td><a href='post_comments.php?delete=$comment_id&id=" . $_GET['id'] ."'>Delete</a></td>";
         echo "</tr>";
 
     }
@@ -108,7 +108,7 @@ if (isset($_GET['approve'])) {
 
     $approve_comment_query = mysqli_query($connection, $query);
 
-    header("Location: post_comments.php");
+    header("Location: post_comments.php?id=" . $_GET['id'] ."");
 
 }
 
@@ -120,7 +120,7 @@ if (isset($_GET['unapprove'])) {
 
     $unapprove_comment_query = mysqli_query($connection, $query);
 
-    header("Location: post_comments.php");
+    header("Location: post_comments.php?id=" . $_GET['id'] ."");
 
 }
 
@@ -132,7 +132,7 @@ if (isset($_GET['delete'])) {
 
     $delete_query = mysqli_query($connection, $query);
 
-    header("Location: post_comments.php");
+    header("Location: post_comments.php?id=" . $_GET['id'] ."");
 
 }
 
